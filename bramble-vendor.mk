5b2c0226b183452df759845e8db2d16e114eabc5
neroices <git@slce.moe>
2024-01-05 09:14:15 +0000

    bramble: Remove DeviceIntelligenceNetworkPrebuilt

--- bramble-vendor.mk
+++ bramble-vendor.mk
@@ -83,7 +83,6 @@ PRODUCT_PACKAGES += \
     ConnMO \
     DCMO \
     DMService \
-    DeviceIntelligenceNetworkPrebuilt \
     DevicePersonalizationPrebuiltPixel2020 \
     DiagMon \
     EuiccGoogle \
